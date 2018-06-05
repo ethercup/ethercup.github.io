60f374b31aa35ace39f3b0d35c40dc174c00e074
Ethercup <mailatethercup@gmail.com>
2018-06-05 10:08:53 +0200

    Added gist sources

--- backend/contracts/BetFactory.sol
+++ backend/contracts/BetFactory.sol
@@ -1,19 +1,9 @@
-// Bytecode origin https://www.reddit.com/r/ethereum/comments/6ic49q/any_assembly_programmers_willing_to_write_a/dj5ceuw/
-// Modified version of Vitalik's https://www.reddit.com/r/ethereum/comments/6c1jui/delegatecall_forwarders_how_to_save_5098_on/
-// Credits to Jordi Baylina for this way of deploying contracts https://gist.github.com/jbaylina/e8ac19b8e7478fd10cf0363ad1a5a4b3
-
-// Forwarder is slightly modified to only return 256 bytes (8 normal returns)
-
-// Deployed Factory in Kovan: https://kovan.etherscan.io/address/0xaebc118657099e2110c90494f48b3d21329b23eb
-
-// Example of a Forwarder deploy using the Factory: https://kovan.etherscan.io/tx/0xe995dd023c8336685cb819313d933ae8938009f9c8c0e1af6c57b8be06986957
-// Just 66349 gas per contract
-
 pragma solidity ^0.4.24;
 
 contract BetFactory {
     event ForwarderDeployed(address forwarderAddress, address targetContract);
 
+    // from: https://gist.github.com/izqui/7f904443e6d19c1ab52ec7f5ad46b3a8
     function createBet(address target) returns (address fwdContract) {
         bytes32 b1 = 0x602e600c600039602e6000f33660006000376101006000366000730000000000; // length 27 bytes = 1b
         bytes32 b2 = 0x5af41558576101006000f3000000000000000000000000000000000000000000; // length 11 bytes
@@ -32,6 +22,7 @@ contract BetFactory {
         ForwarderDeployed(fwdContract, target);
     }
 
+    // from: https://gist.github.com/GNSPS/ba7b88565c947cfd781d44cf469c2ddb
     function createBet2(address _target, bytes _data)
         public
         returns (address proxyContract)
